2e577131806d9ce2859b4756cd3b7b5abd45e079
Alex <alexchizhov90@gmail.com>
2020-01-01 10:40:43 +0300

    Превая часть финального проекта. Верстка по макету и обработка клика по карточке.

---
+++ script.js
@@ -0,0 +1,16 @@
+﻿var main = document.querySelector('#main');
+main.addEventListener( 'click', function(event) {
+
+    // Передняя сторона карточки
+    if (event.target.className === 'flip-card-front') {
+        event.target.parentNode.classList.toggle('is-flipped');
+    }
+    // Задняя сторона карточки
+    else if (event.target.className === 'emoji') {
+        event.target.parentNode.parentNode.classList.toggle('is-flipped');
+    }
+    else if (event.target.className === 'flip-card-back') {
+        event.target.parentNode.classList.toggle('is-flipped');
+    }
+
+});
